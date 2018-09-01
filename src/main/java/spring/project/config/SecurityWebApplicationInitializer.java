c565e240a015dd0d2488e55755a78808827c85ab
Akash Chauhan <akashchauhan.sejapan@gmail.com>
2018-09-01 12:11:44 +0530

    Add files via upload

---
+++ src/main/java/spring/project/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package spring.project.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+      extends AbstractSecurityWebApplicationInitializer {
+}
